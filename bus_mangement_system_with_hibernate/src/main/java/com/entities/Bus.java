1c539ee4aba129f631ad4514043789f6a8150c9d
abhisheksharma2700 <abhisharmahundred@gmail.com>
2025-12-02 13:58:40 +0530

    local files commit

--- bus_mangement_system_with_hibernate/src/main/java/com/entities/Bus.java
+++ bus_mangement_system_with_hibernate/src/main/java/com/entities/Bus.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.entities;
 
 import javax.persistence.Entity;
@@ -79,3 +80,86 @@ public class Bus {
         this.time = time;
     }
 }
+=======
+package com.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Bus {
+    @Id
+    private int bus_id;
+    private String bus_number;
+    private String source;
+    private String destination;
+    private int total_seats;
+    private int available_seats;
+    private String  date;
+    private String time;
+
+    public int getBus_id() {
+        return bus_id;
+    }
+
+    public void setBus_id(int bus_id) {
+        this.bus_id = bus_id;
+    }
+
+    public String getBus_number() {
+        return bus_number;
+    }
+
+    public void setBus_number(String bus_number) {
+        this.bus_number = bus_number;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getTotal_seats() {
+        return total_seats;
+    }
+
+    public void setTotal_seats(int total_seats) {
+        this.total_seats = total_seats;
+    }
+
+    public int getAvailable_seats() {
+        return available_seats;
+    }
+
+    public void setAvailable_seats(int available_seats) {
+        this.available_seats = available_seats;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String  date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
+>>>>>>> 934bed4492ad2153ca2d802f32523505981226ed
--- bus_mangement_system_with_hibernate/src/main/webapp/index.jsp
+++ bus_mangement_system_with_hibernate/src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 <html>
 <body>
 <h2>Hello World!</h2>
 </body>
 </html>
+=======
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+>>>>>>> 934bed4492ad2153ca2d802f32523505981226ed
